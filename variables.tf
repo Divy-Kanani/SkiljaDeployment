adbbace56afe118388ce0ef7f9fb68c6a57ac24a
Divy Kanani <Divy.Kanani@hyland.com>
2023-06-24 18:01:44 -0400

    feat: test

--- variables.tf
+++ variables.tf
@@ -2,6 +2,11 @@
 variable "vpc_cidr" {
   default = "10.0.0.0/16"
 }
+
+variable "vpc_id" {
+  description = "ID of the VPC"
+}
+
 # Defining CIDR Block for 1st Subnet
 variable "subnet_cidr" {
   default = "10.0.1.0/24"
@@ -18,3 +23,4 @@ variable "subnet2_cidr" {
 variable "subnet3_cidr" {
   default = "10.0.4.0/24"
 }
+
